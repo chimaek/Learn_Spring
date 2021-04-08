39db5e273bcb06d20f540c38de60b442243b6ae8
chimaek <pipiru100@gmail.com>
2021-04-08 16:06:40 +0900

    create member by postMapping

---
+++ example/src/main/java/com/learnprojects/example/Controllers/MemberController.java
@@ -0,0 +1,34 @@
+package com.learnprojects.example.Controllers;
+
+import com.learnprojects.example.Domain.Member;
+import com.learnprojects.example.Service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("members/new")
+    public String crateForm(){
+        return "members/crateMemberForm";
+    }
+
+    @PostMapping("members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+}
