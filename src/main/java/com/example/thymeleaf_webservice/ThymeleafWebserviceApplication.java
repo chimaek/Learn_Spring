a36414dddbc9be7614cf28db0ef8a0de7c494b63
chimaek <pipiru100@gmail.com>
2021-06-04 16:52:51 +0900

    create project

---
+++ src/main/java/com/example/thymeleaf_webservice/ThymeleafWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf_webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafWebserviceApplication.class, args);
+	}
+
+}
