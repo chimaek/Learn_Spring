5e7bdb60a49ad6d25db8a7e8f458f8190532ba44
chimaek <pipiru100@gmail.com>
2021-06-14 16:20:49 +0900

    삭제하기 구현

--- react-frontend/src/pages/book/Detail.js
+++ react-frontend/src/pages/book/Detail.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import DeleteIcon from '@material-ui/icons/Delete';
 const Detail = (props) => {
   const id = props.match.params.id;
   const [book, setBook] = useState({
@@ -13,13 +15,30 @@ const Detail = (props) => {
       setBook(res.data);
     });
   }, []);
-
+  const deleteBook = () => {
+    axios.delete('http://localhost:8080/book/' + id).then((res) => {
+      if (res.data === 'ok') {
+        props.history.push('/');
+      }
+    });
+  };
   return (
     <div>
       <h1>책 상세보기</h1>
+      <ButtonGroup variant="contained" color="primary">
+        <Button>수정</Button>{' '}
+        <Button
+          color="secondary"
+          startIcon={<DeleteIcon />}
+          onClick={deleteBook}
+        >
+          삭제
+        </Button>
+      </ButtonGroup>
+
       <hr />
-      <h3>{book.author}</h3>
-      <h1>{book.title}</h1>
+      <h1> 책의 이름은 : {book.title}</h1>
+      <h3> 저자의 이름은 : {book.author}</h3>
     </div>
   );
 };
