9be3d3326dd2382b9317dc799d782cbe49d0dd03
chimaek <pipiru100@gmail.com>
2021-06-21 18:31:34 +0900

    jsp추가 중...

--- springboot_blog/src/main/java/com/example/springboot_blog/domain/User.java
+++ springboot_blog/src/main/java/com/example/springboot_blog/domain/User.java
@@ -25,13 +25,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(nullable = false,length = 30)
+    @Column(nullable = false, length = 30, unique = true)
     private String username;
 
-    @Column(nullable = false,length = 100)
+    @Column(nullable = false, length = 100)
     private String password;
 
-    @Column(nullable = false,length = 50)
+    @Column(nullable = false, length = 50)
     private String email;
 
     @Enumerated(EnumType.STRING)
