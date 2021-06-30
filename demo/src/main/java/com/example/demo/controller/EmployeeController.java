6a58643e93d842078e321c22019984511cecfe6d
chimaek <pipiru100@gmail.com>
2021-06-30 17:53:21 +0900

    get

---
+++ demo/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Employee;
+import com.example.demo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //get all employess
+    @RequestMapping(value = "employess",method = RequestMethod.GET)
+    public List<Employee> employeeList(){
+        return  employeeRepository.findAll();
+    }
+
+
+}
