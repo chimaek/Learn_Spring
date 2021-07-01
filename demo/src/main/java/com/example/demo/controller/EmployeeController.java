edb212eaad6dc6c9609e4c503656b6b49bc777a4
chimaek <pipiru100@gmail.com>
2021-07-01 18:05:49 +0900

    update

--- demo/src/main/java/com/example/demo/controller/EmployeeController.java
+++ demo/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.model.Employee;
 import com.example.demo.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,8 @@ public class EmployeeController {
     private EmployeeRepository employeeRepository;
 
     //get all employess
-    @RequestMapping(value = "employess",method = RequestMethod.GET)
+    @RequestMapping(value = "employees",method = RequestMethod.GET)
+    @CrossOrigin()
     public List<Employee> employeeList(){
         return  employeeRepository.findAll();
     }
