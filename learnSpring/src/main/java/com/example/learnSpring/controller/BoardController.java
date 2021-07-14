e6d885afdce8097378a6c523fc4d7861d84b6867
chimaek <pipiru100@gmail.com>
2021-07-14 18:34:35 +0900

    스프링 시큐리티 추가 및 타임리프 기능 구현

--- learnSpring/src/main/java/com/example/learnSpring/controller/BoardController.java
+++ learnSpring/src/main/java/com/example/learnSpring/controller/BoardController.java
@@ -1,12 +1,19 @@
 package com.example.learnSpring.controller;
 
 import com.example.learnSpring.auth.PrincipalDetail;
+import com.example.learnSpring.model.Board;
+import com.example.learnSpring.model.User;
 import com.example.learnSpring.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class BoardController {
@@ -18,15 +25,21 @@ public class BoardController {
     private BoardService boardService;
 
     @GetMapping({"/", ""})
-    public String index(Model model) {
-        model.addAttribute("boards",boardService.boardList());
-        System.out.println(boardService.boardList());
+    public String index(Model model,@PageableDefault(size = 3, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<Board> userPage = boardService.boardList(pageable);
+        model.addAttribute("boards",userPage);
+//        System.out.println(boardService.boardList());
         return "index";
     }
 
     @GetMapping("/board/saveForm")
     public String saveForm(){
         return "board/saveForm";
+    }
+    @GetMapping("/board/{id}")
+    public String findById(@PathVariable int id,Model model){
+        model.addAttribute("board",boardService.seeContent(id));
+        return "board/detail";
     }
 
 
--- learnSpring/src/main/java/com/example/learnSpring/service/BoardService.java
+++ learnSpring/src/main/java/com/example/learnSpring/service/BoardService.java
@@ -3,7 +3,12 @@ package com.example.learnSpring.service;
 import com.example.learnSpring.model.Board;
 import com.example.learnSpring.model.User;
 import com.example.learnSpring.repository.BoardRepository;
+import net.bytebuddy.TypeCache;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,14 +21,22 @@ public class BoardService {
     private BoardRepository boardRepository;
 
     @Transactional
-    public void writeBoard(Board board, User user){
+    public void writeBoard(Board board, User user) {
         board.setCount(0);
         board.setUser(user);
         boardRepository.save(board);
     }
-    @Transactional(readOnly = true)
-    public List<Board> boardList(){
 
-        return boardRepository.findAll();
+    @Transactional(readOnly = true)
+    public Page<Board> boardList(Pageable pageable) {
+
+        return boardRepository.findAll(pageable);
+    }
+
+    @Transactional(readOnly = true)
+    public Board seeContent(int id) {
+        return boardRepository.findById(id).orElseThrow(() -> {
+            return new IllegalArgumentException("아이디를 찾지못했습니다.");
+        });
     }
 }
