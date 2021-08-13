f3744c74534617509d023475ac5be162b37dada3
chimaek <pipiru100@gmail.com>
2021-08-13 10:23:39 +0900

    프록시를 사용하여 리퀘스트 스코프 활용

--- core/src/main/java/com/hello/core/web/LogDemoController.java
+++ core/src/main/java/com/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package com.hello.core.web;
 
 import com.hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
